617733dccfba730bdf1552c28977d379f673568e
breno-fig <brenoicarocf@gmail.com>
2026-09-09 19:48:04 -0300

    updates on main

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from .schemas import URL_info #needed to have a Request Body parameter
 from sqlalchemy.orm import Session 
 from . import models, schemas
 from .database import SessionLocal, engine
+from .config import get_settings
 
 import validators
 from fastapi import FastAPI, HTTPException, Depends, Request
@@ -10,6 +11,9 @@ from fastapi.responses import RedirectResponse
 
 
 app = FastAPI()
+
+settings = get_settings() #loads the settings from the .env
+
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -49,7 +53,22 @@ def forward_to_url(
     else:
         raise_not_found(request)
 
-@app.post("/unuse/{secret_key}", description="Sets the is_active attribute of the URL object that matches the secret key to False, making it inactive.")
+
+
+@app.post("/url", response_model=URL_info, description="Creates a new URL object in the database and returns it.") #response model is the model that will be returned to the user
+def shorten_url(url: schemas.URL_base, db: Session = Depends(get_db)):
+    if not validators.url(url.target_url): #checks if its a valid URL
+        return raise_bad_request(message="URL not valid")
+    
+    db_url = crud.create_db_url(db=db, url=url) #creates the URL in the database, updates the database and returns the URL object
+    db_url.url = db_url.key 
+    db_url.admin_url = db_url.secret_key
+    
+    return db_url
+
+
+
+@app.post("/unuse/{secret_key}", description="Sets the is_active attribute of the URL to False, if it exists")
 def admin_unuse_url(secret_key: str, request: Request, db: Session = Depends(get_db)):
     db_url = crud.get_db_url_by_secret_key(db=db, secret_key=secret_key)
     if not db_url:
@@ -59,26 +78,26 @@ def admin_unuse_url(secret_key: str, request: Request, db: Session = Depends(get
     return {"message": f"URL with key '{db_url.key}' is now inactive"}
 
 
+@app.post("/reuse/{secret_key}", description="Sets the is_active attribute of the URL to True, if it exists")
+def admin_reuse_url(secret_key: str, request: Request, db: Session = Depends(get_db)):
+    db_url = crud.get_db_url_by_secret_key(db=db, secret_key=secret_key)
+    if not db_url:
+        raise_not_found(request)
+    db_url.is_active = True
+    db.commit()
+    return {"message": f"URL with key '{db_url.key}' is now active"}
 
-@app.delete("/url/{secret_key}", description="Deletes the URL object from the database that matches the secret key.")
-def admin_delete_url(secret_key: str, request = Request, db: Session = Depends(get_db)):
-    if not crud.get_db_url_by_secret_key(db=db, secret_key=secret_key):
+
+@app.delete("/url/{secret_key}", description="Deletes the URL object from the database")
+def admin_delete_url(secret_key: str, request: Request, db: Session = Depends(get_db)):
+    url = crud.get_db_url_by_secret_key(db=db, secret_key=secret_key)
+
+    if not url:
         raise_not_found(request)
 
-    url_key = crud.get_db_url_by_secret_key(db=db, secret_key=secret_key).key    
-    crud.delete_db_url(db=db, url_key=url_key)
-    return {"message": f"URL with key '{url_key}' deleted successfully"}
+    crud.delete_db_url(db=db, url_key=url.key)
+    return {"message": f"URL with key '{url.key}' deleted successfully"}
 
 
 
-@app.post("/url", response_model=URL_info, description="Creates a new URL object in the database and returns it.") #response model is the model that will be returned to the user
-def create_url(url: schemas.URL_base, db: Session = Depends(get_db)):
-    if not validators.url(url.target_url): #checks if its a valid URL
-        return raise_bad_request(message="URL not valid")
-    
-    db_url = crud.create_db_url(db=db, url=url) #creates the URL in the database, updates the database and returns the URL object
-    db_url.url = db_url.key 
-    db_url.admin_url = db_url.secret_key
-    
-    return db_url
 
